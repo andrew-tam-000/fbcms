d5d9d91b1eb2961df18634ff72da73b73d199f90
Andrew Tam <andrew.tam.000@gmail.com>
2017-10-02 20:21:46 -0400

    Adding fake store data

--- src/js/core/redux/store.js
+++ src/js/core/redux/store.js
@@ -13,7 +13,49 @@ const createStoreWithFirebase = compose(
     reactReduxFirebase(firebaseApp, rrfConfig)
 )(createStore);
 
-const store = createStoreWithFirebase(rootReducer, {});
+
+const store = createStoreWithFirebase(rootReducer, {
+    test: {
+        wow: 'one'
+    },
+    firebase: {
+        data: {
+            pages: {
+                asd: {
+                    id: 'asd',
+                    name: 'Page name homepage',
+                    template: 'standard'
+                }
+
+            },
+            templates: {
+                standard: {
+                    fields: {
+                        content: {
+                            id: 'content',
+                            type: 'text'
+                        },
+                        date: {
+                            id: 'date',
+                            type: 'text'
+                        },
+                        title: {
+                            id: 'title',
+                            type: 'text'
+                        }
+                    }
+                }
+            },
+            pageContent: {
+                asd: {
+                    content: 'Test content',
+                    date: '201701323',
+                    title: 'This is a test titel'
+                }
+            }
+        }
+    }
+});
 
 window.store = store;
 
